cf8d9b9cea766197619acef5d04eb4f31f55c2cb
anuj1325 <anujprataps892@gmail.com>
2024-06-08 15:06:33 +0530

    completed project

---
+++ src/components/GlobalComponents/Title.js
@@ -0,0 +1,30 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+
+const Title = ({ title, desc }) => (
+  <div css={styles} className="title">
+    <h2>{title}</h2>
+    <p>{desc}</p>
+  </div>
+);
+
+const styles = css`
+  text-align: center;
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  h2 {
+    font-size: 40px;
+    line-height: 1;
+    color: #fff;
+  }
+  p {
+    font-size: 16px;
+    color: #aab1b7;
+    line-height: 1.5;
+    margin: 10px 0 0 0;
+  }
+`;
+
+export default Title;
